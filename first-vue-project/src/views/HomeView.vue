89cb79f47d944b948d7a253883b9704ad0b4faa1
abdelrahman-abobakr <abdelrahmanabobakr321@gamil.com>
2024-01-13 01:44:47 +0200

    update

--- first-vue-project/src/views/HomeView.vue
+++ first-vue-project/src/views/HomeView.vue
@@ -1,12 +1,13 @@
 <script setup>
  
   import TabbableTextarea from '@/components/TabbableTextarea.vue';
+  import TheWelcomeVue from '@/components/TheWelcome.vue';
   
 </script>
 
 <template>
   <main>
-
+    <TheWelcomeVue/>
     <form>
         <TabbableTextarea style="width:100%; height:300px;" />
     </form>
